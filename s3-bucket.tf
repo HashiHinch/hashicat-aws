72663836df90d18a8ec4115eef5d7c154954989e
Craig Hinchliffe <you@example.com>
2022-10-31 23:26:27 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,9 @@ module "s3-bucket" {
   source  = "HashiHinch/s3-bucket/aws"
   version = "2.8.0"
   bucket = "s3-bucket"
-  s3_bucket_id = "Guarav"
+    acl    = "private"
+    versioning = {
+    enabled = true
+  }
+  
 }
