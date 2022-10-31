97da560df5d95e4cfba63b4b3c16935fb97d22d7
Craig Hinchliffe <you@example.com>
2022-10-31 22:44:14 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "aws_vpc" "hashicat" {
   tags = {
     name = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
-    Department = "devops"
-    Billable = "true"
+    devops = "Department"
+    true = "Billable"
   }
 
 
